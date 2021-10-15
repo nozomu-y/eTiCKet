031c90768beadc2226209c78370b658a14adfdca
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-16 00:09:13 +0900

    Update: route for home

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
