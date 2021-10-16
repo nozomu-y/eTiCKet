aa2e63f01ddee6e94aff3ffaa8c08c932cf2cb91
nozoyama39 <nozoyama39@gmail.com>
2021-10-17 00:07:21 +0900

    Update: lang file

--- resources/views/events/detail.blade.php
+++ resources/views/events/detail.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main')
+@section('title', $event->name)
 
 @section('content')
     <div class="container-fluid">
-        <h1 class="h3 text-gray-800 mb-4">イベント</h1>
+        <h1 class="h3 text-gray-800 mb-4">{{ $event->name }}</h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('events') }}">イベント一覧</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('events') }}">{{ __('event_list') }}</a></li>
                 <li class="breadcrumb-item active" aria-current="page">{{ $event->name }}</li>
             </ol>
         </nav>
@@ -13,7 +14,7 @@
             <div class="col-lg-4">
                 <div class="card mb-4">
                     <div class="card-header">
-                        イベント情報
+                        {{ __('event_information') }}
                     </div>
                     <div class="card-body">
                         <h4>{{ $event->name }}</h4>
@@ -21,18 +22,18 @@
                             {{ $event->date }} @ {{ $event->place }}
                             @if ($event->open_at != null)
                                 <br>
-                                開場：{{ $event->open_at }}
+                                {{ __('open_at_abbrev') }}：{{ $event->open_at }}
                             @endif
                             @if ($event->start_at != null)
                                 <br>
-                                開演：{{ $event->start_at }}
+                                {{ __('start_at_abbrev') }}：{{ $event->start_at }}
                             @endif
                             @if ($event->end_at != null)
                                 <br>
-                                終演：{{ $event->end_at }}
+                                {{ __('end_at_abbrev') }}：{{ $event->end_at }}
                             @endif
                             <br>
-                            有効期限：{{ $event->expire_at }}
+                            {{ __('expire_at') }}：{{ $event->expire_at }}
                         </p>
                     </div>
                 </div>
@@ -44,11 +45,11 @@
                     <a class="list-group-item list-group-item-action">{{ __('ticket_list') }}</a>
                     <a class="list-group-item list-group-item-action">{{ __('edit_event') }}</a>
                     <a class="list-group-item list-group-item-action text-danger"
-                       onclick="if (confirm('{{ __('message.confirm.event.delete') }}')) {event.preventDefault(); document.getElementById('delete-form').submit();}"
-                        >{{ __('delete_event') }}</a>
-                    <form id="delete-form" action="{{ route('delete_event', ['id' => $event->event_id]) }}" method="POST" class="d-none">
-                    @csrf
-                </form>
+                        onclick="if (confirm('{{ __('message.confirm.event.delete') }}')) {event.preventDefault(); document.getElementById('delete-form').submit();}">{{ __('delete_event') }}</a>
+                    <form id="delete-form" action="{{ route('delete_event', ['id' => $event->event_id]) }}" method="POST"
+                        class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
