67438ccbb5f9bdec6e48e13e02fc4d73258ffdfc
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-17 00:59:39 +0900

    Update: lang file

--- resources/views/accounts/add.blade.php
+++ resources/views/accounts/add.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.main')
+@section('title', __('add_account'))
 
 @section('content')
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Add_Account') }}</div>
+                    <div class="card-header">{{ __('add_account') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('post_add_account') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('name') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
@@ -28,7 +29,7 @@
 
                             <div class="form-group row">
                                 <label for="username"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('username') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="username" type="username"
@@ -45,7 +46,7 @@
 
                             <div class="form-group row">
                                 <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -62,7 +63,7 @@
 
                             <div class="form-group row">
                                 <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('password_confirm') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -73,7 +74,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Add') }}
+                                        {{ __('add') }}
                                     </button>
                                 </div>
                             </div>
