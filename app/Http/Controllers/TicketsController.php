3fbc675d49667ffd4a8fce29fb158bafd7a72b0b
nozoyama39 <nozoyama39@gmail.com>
2021-10-21 16:26:08 +0900

    Add: ticket list

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Events;
+use App\Models\Tickets;
 
 class TicketsController extends Controller
 {
-    //
+    function index($event_id) {
+        $event = Events::where('event_id', $event_id)->get()->first();
+        $tickets = Tickets::where('event_id', $event_id)->get();
+        return view('tickets.list', [
+            'event' => $event,
+            'tickets' => $tickets]
+        );
+    }
 }
