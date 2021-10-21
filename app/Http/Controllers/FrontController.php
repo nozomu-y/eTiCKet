3c8a1bbfa8731fdfc8ae2f3ced396a491874f1d4
nozoyama39 <nozoyama39@gmail.com>
2021-10-21 22:42:46 +0900

    Update: show ticket data

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Events;
+use App\Models\Tickets;
 
 class FrontController extends Controller
 {
@@ -18,5 +20,15 @@ class FrontController extends Controller
         $event_id = $url_split[count($url_split)-3];
         $ticket_id = $url_split[count($url_split)-2];
         $token = $url_split[count($url_split)-1];
+        $event = Events::where('event_id', $event_id)->get()->first();
+        $ticket = Tickets::where('event_id', $event_id)->where('ticket_id', $ticket_id)->get()->first();
+        if ($token !== $ticket->token) {
+            dd('error');
+        }
+        dump($event->name);
+        dump($ticket->ticket_id);
+        dump($ticket->seat);
+        dump($ticket->door);
+        dd($token);
     }
 }
