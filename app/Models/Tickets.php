e94f7584f7d0b076f0b8cdcab47fd8b31075ceb9
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-21 16:20:19 +0900

    Add: model for tickets

---
+++ app/Models/Tickets.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tickets extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tickets';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ticket_id';
+}
