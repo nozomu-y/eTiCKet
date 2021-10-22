3eea546ee7b16d1bd30011a77bfea8e96411b0d3
nozoyama39 <nozoyama39@gmail.com>
2021-10-22 18:07:07 +0900

    Update: change login page ui

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -7,19 +7,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-3"></div>
-                            <div class="col-lg-6">
-
-                                @yield('content')
-
-                            </div>
-                            <div class="col-lg-3"></div>
-                        </div>
-                    </div>
-                </div>
+                @yield('content')
             </div>
         </div>
     </div>
