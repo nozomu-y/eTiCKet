b777632f6bca6c453352ad304d0bb35da5f2f09c
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-22 19:43:00 +0900

    Update: ticket view

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -1,3 +1,6 @@
+<?php
+use App\Libs\Common;
+?>
 @extends('layouts.main')
 @section('title', $event->name)
 @section('style')
@@ -11,6 +14,21 @@
 
 @section('content')
     <div class="container-fluid">
+        @if (url()->previous() === route('tickets', ['event_id' => $event->event_id]))
+            <h1 class="h3 text-gray-800 mb-4">{{ __('ticket_detail') }}</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('events') }}">{{ __('event_list') }}</a></li>
+                    <li class="breadcrumb-item"><a
+                            href="{{ route('event_detail', ['event_id' => $event->event_id]) }}">{{ $event->name }}</a>
+                    </li>
+                    <li class="breadcrumb-item"><a
+                            href="{{ route('tickets', ['event_id' => $event->event_id]) }}">{{ __('ticket_list') }}</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ __('ticket_detail') }}</li>
+                </ol>
+            </nav>
+        @endif
         <div class="row">
             <div class="col-lg-4">
                 <div class="card mb-4">
@@ -44,17 +62,22 @@
                         <strong>{{ $seat_type }}</strong>
                         <br>
                         @if ($ticket->seat != null)
-                        <strong class="mr-1">{{ __('seat_no') }}</strong>
-                        {{ $ticket->seat }}
-                        <br>
+                            <strong class="mr-1">{{ __('seat_no') }}</strong>
+                            {{ $ticket->seat }}
+                            <br>
                         @endif
                         @if ($ticket->door != null)
-                        <strong class="mr-1">{{ __('door_no') }}</strong>
-                        {{ $ticket->door}}
-                        <br>
+                            <strong class="mr-1">{{ __('door_no') }}</strong>
+                            {{ $ticket->door }}
+                            <br>
                         @endif
                         <strong class="mr-1">{{ __('price') }}</strong>
-                        {{ $ticket->price }}
+                        {{ Common::format_price($ticket->price) }}
+                    </p>
+                    <p>
+                        <strong class="mr-1">{{ __('ticket_url') }}</strong>
+                        <a href="{{ config('app.url') . '/' . $event->event_id . '/' . $ticket->ticket_id . '/' . $ticket->token }}"
+                            class="text-break">{{ config('app.url') . '/' . $event->event_id . '/' . $ticket->ticket_id . '/' . $ticket->token }}</a>
                     </p>
                 </div>
             </div>
