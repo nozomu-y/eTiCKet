23cd82a1610aeae688cb8be1970bf64ff8abc3ae
nozoyama39 <nozoyama39@gmail.com>
2021-10-22 19:21:03 +0900

    Update: layouts

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.guest')
+@extends('layouts.main')
 @section('title', '404 Page Not Found')
 
 @section('content')
     <div class="container-fluid">
         <div class="text-center">
-            <div class="error mx-auto" data-text="404">404</div>
+            <div class="display-1" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Page Not Found</p>
+            <a href="{{route('home')}}">Take Me Home</a>
         </div>
     </div>
 @endsection
