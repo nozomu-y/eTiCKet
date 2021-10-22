10bf768eafac86b778b72f625ec3120c77d58207
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-22 18:07:07 +0900

    Update: change login page ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.login')
-
 @section('title', __('login'))
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
 
 @section('content')
     <div class="p-5">
         <div class="text-center">
-            <h1 class="h4 text-gray-900 mb-4">{{ config('app.name') }}</h1>
+            <h1 class="h3 text-gray-900 mb-4">{{ config('app.name') }}</h1>
         </div>
 
-        {{ Form::open(['url' => 'login', 'method' => 'post', 'class' => 'user']) }}
+        {{ Form::open(['url' => 'login', 'method' => 'post']) }}
         <div class="form-group">
             {{ Form::text('username', null, [
-                'class' => 'form-control form-control-user' . ($errors->has('username') ? ' is-invalid' : ''),
+                'class' => 'form-control' . ($errors->has('username') ? ' is-invalid' : ''),
                 'autocomplete' => 'username',
                 'id' => 'username',
                 'placeholder' => __('username'),
@@ -26,7 +29,7 @@
         </div>
         <div class="form-group">
             {{ Form::password('password', [
-                'class' => 'form-control form-control-user' . ($errors->has('password') ? ' is-invalid' : ''),
+                'class' => 'form-control' . ($errors->has('password') ? ' is-invalid' : ''),
                 'autocomplete' => 'current-password',
                 'id' => 'password',
                 'placeholder' => __('password'),
@@ -38,8 +41,17 @@
                 </span>
             @enderror
         </div>
+        <div class="form-group">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                    {{ old('remember') ? 'checked' : '' }}>
+
+                <label class="form-check-label" for="remember">
+                    {{ __('remember_me') }}
+                </label>
+            </div>
+        </div>
         {{ Form::button(__('login'), ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login', 'type' => 'submit']) }}
         {{ Form::close() }}
     </div>
 @endsection
-
