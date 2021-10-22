45738d45eb6a7e035e0f10614564b86f602f9203
nozoyama39 <nozoyama39@gmail.com>
2021-10-22 21:42:00 +0900

    Update: ticket UI

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -6,7 +6,18 @@ use App\Libs\Common;
 @section('style')
     <style>
         svg {
-            width: 100% !important;
+            width: calc(100% - 20px) !important;
+            border-top-left-radius: calc(.25rem - 1px);
+            border-top-right-radius: calc(.25rem - 1px);
+            font-size: 1.125rem;
+            text-anchor: middle;
+            height: auto;
+            margin: 10px;
+        }
+
+        .ticket {
+            max-width: 400px;
+            min-width: 300px;
         }
 
     </style>
@@ -31,49 +42,91 @@ use App\Libs\Common;
         @endif
         <div class="row">
             <div class="col-lg-4">
-                <div class="card mb-4">
-                    <div class="card-body bg-white">
-                        {!! QrCode::size(300)->generate(config('app.url') . '/' . $event->event_id . '/' . $ticket->ticket_id . '/' . $ticket->token) !!}
+                <div class="d-flex justify-content-center">
+                    <div class="card mb-4 ticket">
+                        {!! QrCode::size(400)->generate(config('app.url') . '/' . $event->event_id . '/' . $ticket->ticket_id . '/' . $ticket->token) !!}
+                        <div class="card-body">
+                            <div class="text-center">
+                                <?php
+                                $lines = explode(' ', config('app.corporation_name'));
+                                ?>
+                                @foreach ($lines as $line)
+                                    <h2 class="h4 mb-0">{{ $line }}</h2>
+                                @endforeach
+                                <h3 class="h5 mt-1">{{ $event->name }}</h3>
+                            </div>
+                            <table class="mt-3" style="border-collapse: separate; border-spacing: 1em 0em;">
+                                <tbody>
+                                    <tr>
+                                        <th class="mr-3 text-nowrap">{{ __('date') }}</th>
+                                        <td>{{ date('Y/m/d', strtotime($event->date)) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-nowrap">{{ __('hall') }}</th>
+                                        <td>{{ $event->place }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="mt-3 d-flex justify-content-center"
+                                style="border-collapse: separate; border-spacing: 1em 0em;">
+                                <tbody>
+                                    <tr>
+                                        @if ($event->open_at != null)
+                                            <th class="text-nowrap">{{ __('open_at_abbrev') }}</th>
+                                            <td>{{ date('H:i', strtotime($event->open_at)) }}</td>
+                                        @endif
+                                        @if ($event->start_at != null)
+                                            <th class="text-nowrap">{{ __('start_at_abbrev') }}</th>
+                                            <td>{{ date('H:i', strtotime($event->start_at)) }}</td>
+                                        @endif
+                                    </tr>
+                                    @if ($event->end_at != null)
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <th class="text-nowrap">{{ __('end_at_abbrev') }}</th>
+                                            <td>{{ date('H:i', strtotime($event->end_at)) }}</td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                            <hr>
+                            <table class="mt-3 d-flex justify-content-center"
+                                style="border-collapse: separate; border-spacing: 1em 0em;">
+                                <tbody>
+                                    <tr>
+                                        <th class="text-nowrap">{{ __('seat_type') }}</th>
+                                        <td>{{ $seat_type }}</td>
+                                        <th class="text-nowrap">{{ __('price') }}</th>
+                                        <td>{{ Common::format_price($ticket->price) }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="mt-1 d-flex justify-content-center"
+                                style="border-collapse: separate; border-spacing: 1em 0em;">
+                                <tbody>
+                                    <tr>
+                                        @if ($ticket->seat != null)
+                                            <th class="text-nowrap">{{ __('seat_no') }}</th>
+                                            <td>{{ $ticket->seat }}</td>
+                                        @endif
+                                        @if ($ticket->door != null)
+                                            <th class="text-nowrap">{{ __('door_no') }}</th>
+                                            <td>{{ $ticket->door }}</td>
+                                        @endif
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="text-center mt-2">
+                                <small class="text-monospace">No. {{ sprintf('%06d', $ticket->ticket_id) }}</small>
+                                <br>
+                                <small class="text-monospace">{{ $ticket->token }}</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="h4">{{ $event->name }}</h2>
-                    <h3 class="h6">{{ $event->place }}</h3>
-                    <h3 class="h6">{{ date('Y/m/d', strtotime($event->date)) }}</h3>
-                    <p>
-                        @if ($event->open_at != null)
-                            <strong class="mr-1">{{ __('open_at_abbrev') }}</strong>
-                            {{ date('H:i', strtotime($event->open_at)) }}
-                            <br>
-                        @endif
-                        @if ($event->start_at != null)
-                            <strong class="mr-1">{{ __('start_at_abbrev') }}</strong>
-                            {{ date('H:i', strtotime($event->start_at)) }}
-                            <br>
-                        @endif
-                        @if ($event->end_at != null)
-                            <strong class="mr-1">{{ __('end_at_abbrev') }}</strong>
-                            {{ date('H:i', strtotime($event->end_at)) }}
-                            <br>
-                        @endif
-                    </p>
-                    <p>
-                        <strong>{{ $seat_type }}</strong>
-                        <br>
-                        @if ($ticket->seat != null)
-                            <strong class="mr-1">{{ __('seat_no') }}</strong>
-                            {{ $ticket->seat }}
-                            <br>
-                        @endif
-                        @if ($ticket->door != null)
-                            <strong class="mr-1">{{ __('door_no') }}</strong>
-                            {{ $ticket->door }}
-                            <br>
-                        @endif
-                        <strong class="mr-1">{{ __('price') }}</strong>
-                        {{ Common::format_price($ticket->price) }}
-                    </p>
                     <p>
                         <strong class="mr-1">{{ __('ticket_url') }}</strong>
                         <a href="{{ config('app.url') . '/' . $event->event_id . '/' . $ticket->ticket_id . '/' . $ticket->token }}"
