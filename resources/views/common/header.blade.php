e770df6a6c36f65f470bf80e7477e26714f8fb40
nozoyama39 <nozoyama39@gmail.com>
2021-10-23 13:55:27 +0900

    Update: navbar

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -13,33 +13,7 @@
         </div>
 
         @if (!Auth::guest())
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user fa-fw"></i>
-                        <span class="mr-2 text-gray-600 small">{{ Auth::user()->name }}</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="{{ route('change_password') }}">
-                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                            {{ __('change_password') }}
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            {{ __('logout') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>
-
-            <div class="collapse navbar-collapse" id="navbar">
+            <div class="collapse navbar-collapse border-top" id="navbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('home') }}">
@@ -67,6 +41,29 @@
                     {{-- {{ __('info') }} --}}
                     {{-- </a> --}}
                     {{-- </li> --}}
+                    <li class="nav-item border-top my-0"></li>
+                    <li class="nav-item">
+                        <span class="nav-link">
+                            <i class="fas fa-user fa-fw mr-2"></i>
+                            {{ Auth::user()->name }}
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ml-4" href="{{ route('change_password') }}">
+                            <i class="fas fa-cogs fa-fw mr-2"></i>
+                            {{ __('change_password') }}
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ml-4" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt fa-fw mr-2"></i>
+                            {{ __('logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
                 </ul>
             </div>
         @endif
