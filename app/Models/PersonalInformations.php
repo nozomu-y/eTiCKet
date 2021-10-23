285507db1e5b21be46297a33ec336c9136acb754
nozoyama39 <nozoyama39@gmail.com>
2021-10-23 21:47:01 +0900

    Add: create model PersonalInformations

---
+++ app/Models/PersonalInformations.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PersonalInformations extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'personal_informations';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = ['event_id', 'ticket_id'];
+}
