21b90fbd3e2b8f39d62cfb9268c2a967ac77b0ce
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-25 14:49:12 +0900

    Fix: show message after redirection

--- resources/views/tickets/links.blade.php
+++ resources/views/tickets/links.blade.php
@@ -13,9 +13,9 @@
     </nav>
     <div class="row">
         <div class="col-12">
-            @if (session('success'))
+            @if (isset($success))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{ session('success') }}
+                    {{ $success }}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
