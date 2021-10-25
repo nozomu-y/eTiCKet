66a1069722a3dbdf996862e252de3ce26888754b
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-10-25 14:15:36 +0900

    Update: table seeder for events and tickets

--- database/seeders/EventsTableSeeder.php
+++ database/seeders/EventsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Enums\SeatType;
+use App\Enums\CollectType;
 
 class EventsTableSeeder extends Seeder
 {
@@ -14,11 +16,26 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('events')->insert([
-            'name' => 'Test Concert',
-            'place' => 'Test Hall',
-            'date' => '2099-01-01',
-            'expire_at' => '2099-01-01 23:59',
+        $event_id = DB::table('events')->insertGetId([
+            'name' => 'クリスマスコンサート（デモ）',
+            'place' => '講堂',
+            'date' => '2021-12-25',
+            'expire_at' => '2021-12-25 23:59',
+            'seat_type' => SeatType::RESERVED,
+            'collect_name' => CollectType::REQUIRED,
+            'collect_email' => CollectType::OPTIONAL,
+            'collect_phone_number' => CollectType::OPTIONAL,
         ]);
+        $seats = ['A1-1','A1-2','A1-3','A1-4','A1-5'];
+        $ticket_id = 0;
+        foreach ($seats as $seat) {
+            $ticket_id = $ticket_id + 1;
+            DB::table('tickets')->insert([
+                'ticket_id' => $ticket_id,
+                'event_id' => $event_id,
+                'seat' => $seat,
+                'price' => 1200,
+            ]);
+        }
     }
 }
