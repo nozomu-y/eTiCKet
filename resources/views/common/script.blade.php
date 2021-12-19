5281affeadfa66e99a848327b83e8a12f2d6ee31
Nozomu Yamazaki <nozoyama39@gmail.com>
2021-12-19 16:20:17 +0900

    Update: rename js file

--- resources/views/common/script.blade.php
+++ resources/views/common/script.blade.php
@@ -7,7 +7,7 @@
 <!-- Core plugin JavaScript-->
 
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('js/chart.min.js') }}"></script>
+<script src="{{ asset('js/Chart.min.js') }}"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script type="text/javascript"
